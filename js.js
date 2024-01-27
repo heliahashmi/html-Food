e95780b21403c500e2d39b126fb79d3638b567bc
mobina <mobinakhazaei2590@gmail.com>
2024-01-27 10:09:15 -0800

    add js function for form

--- js.js
+++ js.js
@@ -1,1 +1,7 @@
 window.alert("Welcome To Order Site");
+
+function price(){
+    var a=Number(document.getElementById("txt1").value);
+    var b=Number(document.getElementById("txt2").value);
+    document.getElementById("txt3").value=a+b;
+}
